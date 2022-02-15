b394ca0c5ed00b5897082a88688d322f86c674fc
adamcg123 <adam.Gondorchin26@gmail.com>
2022-02-15 16:48:56 -0600

    server file set up

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const fs = require("fs");
+const notes = require("./Develop/db/db.json");
+const path = require("path");
+const uuid = require("uuid");
+const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
+
+const app = express();
+var PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
